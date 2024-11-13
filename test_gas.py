86326c71f4365f1c627dc054bbe2e351c6e0c737
weiwen <tanweiwen32@gmail.com>
2024-11-13 14:02:38 +0800

    Create test_gas.py

---
+++ test_gas.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+
+# Define the pin connected to the MQ135 DO
+mq135_digital_pin = 27
+
+# Set up the pin as input
+GPIO.setup(mq135_digital_pin, GPIO.IN)
+try:
+    while True:
+        gas_detected = GPIO.input(mq135_digital_pin)
+        if gas_detected:
+            print("High Gas Levels Detected!")
+        else:
+            print("Gas Levels Are Normal.")
+finally:
+    print("Cleaning up")
+    GPIO.cleanup()
